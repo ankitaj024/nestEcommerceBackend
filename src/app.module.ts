a9cfc4ea33567f5ba854d8cf27c1f0ec70a76ae2
Ajay <you@example.com>
2025-04-22 10:31:59 +0530

    ajay pull ankit branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [UserModule, PrismaModule],
+  imports: [UserModule, PrismaModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
